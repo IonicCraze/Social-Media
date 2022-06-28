06afc486131cb736fae5bb2ba5846ea0d8f81b7d
IonicCraze <daviddeitz97@gmail.com>
2022-06-28 13:54:15 -0400

    added models

---
+++ models/Thoughts.js
@@ -0,0 +1,34 @@
+const {Schema, model} = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtsSchema = new Schema(
+    {
+    thoughtText: {
+        type: String,
+        required: true,
+        max_length: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+       },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema] ,
+    },
+    {
+    toJSON: {
+        getters: true,
+    },
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+})
+
+const User = model('thoughts', thoughtsSchema);
+
+module.exports = Thoughts;
---
+++ models/User.js
@@ -0,0 +1,42 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address'],
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }
+    ] ,
+    },
+    {
+    toJSON: {
+        getters: true,
+    },
+    }
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+const User = model('user', userSchema);
+
+module.exports = User;
